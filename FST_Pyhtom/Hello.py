c51b7e3d594550bbbef5d6be5637e82280d4f0a3
Mudit Srivastava <mudit.srivastava@ibm.com>
2022-05-21 02:45:29 +0530

    python act

---
+++ FST_Pyhtom/Hello.py
@@ -0,0 +1,15 @@
+print("hello world")
+
+var = "hello"
+var = 10
+print(var)
+
+txt = "My age is {}"
+age = 30
+
+print(txt + str(age))
+a = 2**5
+print(a)
+
+
+
---
+++ FST_Pyhtom/first_test.py
@@ -0,0 +1,6 @@
+import test
+import math
+
+def testmatch():
+    num = 25
+    assert math.sqrt(num) == 5
